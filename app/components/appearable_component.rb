4fd723e3c8aecb9db02190503e818d8bcd8612a8
mario <github@lxxxvi.ch>
2022-08-19 08:19:03 +0200

    wip, remove stuff

--- app/components/appearable_component.rb
+++
@@ -1,7 +0,0 @@
-class AppearableComponent < ApplicationComponent
-  attr_reader :name
-
-  def initialize(name:)
-    @name = name
-  end
-end
--- app/components/closeable_button_component.rb
+++
@@ -1,9 +0,0 @@
-class CloseableButtonComponent < ApplicationComponent
-  renders_one :opening_element
-
-  attr_reader :closeable_after
-
-  def initialize(closeable_after:)
-    @closeable_after = closeable_after
-  end
-end
