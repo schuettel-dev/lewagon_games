bd6c9e19b69665537cb936fbe2a2ab67f31bc6a0
mario <github@lxxxvi.ch>
2022-09-06 18:25:38 +0200

    fix seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,6 +4,8 @@
   User.find_by(nickname: github_nickname).privilege_superadmin!
 end
 
-GameType.find_or_create_by(instance_type: "Game::Beatle") do |game|
+GameType.find_or_initialize_by(instance_type: "Game::Beatle").tap do |game|
   game.name = "Beatle"
+  game.description = "Guess who's behind a playlist of 3 songs"
+  game.save!
 end
