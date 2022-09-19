5b9362529321ef059455c188a45aeb68144a2eea
mario <github@lxxxvi.ch>
2022-09-19 09:52:39 +0200

    add BatchTest

--- app/models/batch.rb
+++ app/models/batch.rb
@@ -8,7 +8,7 @@ class Batch < ApplicationRecord
 
   before_validation :set_url_identifier, if: :new_record?
 
-  validates :name, :location, presence: true
+  validates :name, :location, :owner, presence: true
   validates :name, :url_identifier, uniqueness: true
 
   scope :search, ->(query) {
@@ -18,12 +18,6 @@ class Batch < ApplicationRecord
   }
   scope :ordered_by_name, -> { order(name: :asc) }
 
-  def owner=(user)
-    memberships.find_or_initialize_by(role: :owner).tap do |membership|
-      membership.user = user if membership.new_record?
-    end
-  end
-
   def to_param
     url_identifier
   end
@@ -31,6 +25,6 @@ class Batch < ApplicationRecord
   private
 
   def set_url_identifier
-    self.url_identifier = name.parameterize
+    self.url_identifier = name&.parameterize
   end
 end
--- test/models/batch_test.rb
+++ test/models/batch_test.rb
@@ -1,7 +1,22 @@
 require "test_helper"
 
 class BatchTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "validations" do
+    batch = Batch.new
+    assert_not batch.valid?
+
+    batch.errors.to_a.tap do |errors|
+      assert_includes errors, "Name can't be blank"
+      assert_includes errors, "Location can't be blank"
+      assert_includes errors, "Owner can't be blank"
+    end
+  end
+
+  test "#owner=, new record" do
+    owner = users(:luigi)
+    batch = Batch.new(name: "Batch X", location: "Somewhere")
+    batch.owner = owner
+    assert batch.save
+    assert_equal owner, batch.owner
+  end
 end
