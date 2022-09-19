c0766f62106ae5774237c1ed9815169922fe7097
mario <github@lxxxvi.ch>
2022-09-19 10:20:55 +0200

    add GameTest

--- test/models/game_test.rb
+++ test/models/game_test.rb
@@ -1,7 +1,13 @@
 require "test_helper"
 
 class GameTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "validations" do
+    game = Game.new
+    assert_not game.valid?
+    game.errors.to_a.tap do |errors|
+      assert_includes errors, "Game type must exist"
+      assert_includes errors, "Batch must exist"
+      assert_includes errors, "User must exist"
+    end
+  end
 end
